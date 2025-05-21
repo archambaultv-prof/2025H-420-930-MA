9024ae5333b04b0d8f7d48cbe73a0dd685b62cf7
Vincent Archambault-Bouffard <varchambaultbouffard@cmaisonneuve.qc.ca>
2025-05-21 02:37:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Système d'exploiration - Formation continue - Collège de Maisonneuve
+
+Ce dépôt contient le matériel pédagogique du cours algorithmes et modèles de programmation
+420-930-MA donné au [Collège de Maisonneuve](https://www.cmaisonneuve.qc.ca/)
+par le professeur Vincent Archambault-Bouffard.
+
+## Site web du cours
+- Le site web du cours est accessible via github pages à l'adresse
+  [https://archambaultv-prof.github.io/2025H-420-930-MA](https://archambaultv-prof.github.io/2025H-420-930-MA).
---
+++ docusaurus.config.js
@@ -0,0 +1,168 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '2025H-420-930-MA',
+  tagline: "Systèmes d'exploitation",
+  // favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://archambaultv-prof.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/2025H-420-930-MA/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'archambaultv-prof', // Usually your GitHub org/user name.
+  projectName: '2025H-420-930-MA', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          routeBasePath: '/',
+          editUrl:
+            'https://github.com/archambaultv-prof/2025H-420-930-MA/tree/main/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+  
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      // image: 'img/docusaurus-social-card.jpg',
+      docs:{
+        sidebar: {
+          hideable: true,
+          autoCollapseCategories: true,
+        }
+      },
+      navbar: {
+        title: "Système d'exploitation",
+        logo: {
+          alt: 'Site Logo',
+          src: 'img/930.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Accueil',
+          },
+          {
+            href: 'https://github.com/archambaultv-prof/2025H-420-930-MA/',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: '669M0YO6L1',
+
+        // Public API key: it is safe to commit it
+        apiKey: '82af2388064e1ea75126c600b48cf0a4',
+
+        indexName: 'archambaultv-profio',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        insights: false,
+
+      },
+      footer: {
+        style: 'dark',
+        // links: [
+        //   {
+        //     title: 'Docs',
+        //     items: [
+        //       {
+        //         label: 'Tutorial',
+        //         to: '/docs/intro',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'Community',
+        //     items: [
+        //       {
+        //         label: 'Stack Overflow',
+        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //       },
+        //       {
+        //         label: 'Discord',
+        //         href: 'https://discordapp.com/invite/docusaurus',
+        //       },
+        //       {
+        //         label: 'X',
+        //         href: 'https://x.com/docusaurus',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'More',
+        //     items: [
+        //       {
+        //         label: 'Blog',
+        //         to: '/blog',
+        //       },
+        //       {
+        //         label: 'GitHub',
+        //         href: 'https://github.com/facebook/docusaurus',
+        //       },
+        //     ],
+        //   },
+        // ],
+        copyright: `Cette œuvre est placée sous licence <a href="https://creativecommons.org/licenses/by/4.0/deed.fr" target="_blank" rel="noopener noreferrer">Creative Commons Attribution 4.0 International</a>. Construit avec Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
